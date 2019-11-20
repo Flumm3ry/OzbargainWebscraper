068821fd1624370f95fdde65486e16d2bfe8fadb
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-11-21 07:42:10 +1100

    Fixed whitespace issues found by flake8

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from scraper import Scraper
 
 scraper = Scraper("https://www.ozbargain.com.au/")
 
-print (scraper.count())
-print (scraper.nodes[0])
+print(scraper.count())
+print(scraper.nodes[0])
