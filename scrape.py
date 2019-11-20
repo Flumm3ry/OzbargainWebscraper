a2bbb26c783a6c33b1dfb6cddceddcaf10bc57a7
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-11-21 06:35:44 +1100

    Fixed issues with whitespace identified by flake8

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.ozbargain.com.au/'
 response = get(url, headers={'User-Agent': 'Mozilla/5.0'})
-html= response.content
+html = response.content
 
 soup = BeautifulSoup(html)
 data = soup.findAll('div', attrs={'id': lambda L: L and L.startswith("node")})
--- scraper_test.py
+++ scraper_test.py
@@ -1,5 +1,6 @@
 def test_sample1():
     assert True
 
+
 def test_sample2():
-    assert 1==1
+    assert 1 == 1
