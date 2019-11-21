e8dad2ab54176089beff586fb3af958944e15af7
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-11-21 20:58:09 +1100

    Added tests to ensure all cells have content in them and there is not html in the extracted data

--- scraper_test.py
+++ scraper_test.py
@@ -1,14 +1,26 @@
 from scraper import Scraper
+from bs4 import BeautifulSoup
 
 url = 'https://www.ozbargain.com.au/'
+scraper = Scraper(url)
 
 
 def test_count():
-    scraper = Scraper(url)
     assert scraper.count() > 0
 
 
 def test_does_not_expired():
-    scraper = Scraper(url)
     for node in scraper.nodes:
         assert str(node).find("expired") == -1
+
+
+def test_all_cells_populated():
+    for row in scraper.table:
+        assert row[0]  # title
+        assert row[1]  # content
+
+
+def test_not_html_in_content():
+    for row in scraper.table:
+        assert not bool(BeautifulSoup(row[0], 'html.parser').find())
+        assert not bool(BeautifulSoup(row[1], 'html.parser').find())
