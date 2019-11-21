14c6481033a6e2b43ccc719d96cee12c44a041d1
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-11-21 20:37:25 +1100

    Scraper now extracts the title and content of each listing and puts it into a list of lists

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from scraper import Scraper
 scraper = Scraper("https://www.ozbargain.com.au/")
 
 print(scraper.count())
-print(scraper.nodes[0])
+scraper.print_table()
--- scraper.py
+++ scraper.py
@@ -8,9 +8,14 @@ class Scraper:
         response = get(url, headers={'User-Agent': 'Mozilla/5.0'})
         html = response.content
         self.data = BeautifulSoup(html, features='html.parser')
-        self.nodes = self.extractNodes(self.data)
+        self.nodes = self.extractRow(self.data)
 
-    def extractNodes(self, data):
+        self.table = []
+
+        for node in self.nodes:
+            self.table.append(self.extractData(node))
+
+    def extractRow(self, data):
         return data.findAll(
                 'div',
                 attrs={'class': lambda L: L
@@ -18,5 +23,22 @@ class Scraper:
                        and L.find("expired") == -1}
                 )
 
+    def extractData(self, node):
+        result = []
+        title_section = node.find('h2', attrs={'class': 'title'})
+        title = title_section.get('data-title')
+        result.append(title)
+        content_section = node.find('div', attrs={'class': 'content'})
+        content = content_section.get_text().strip()
+        result.append(content)
+        return result
+
     def count(self):
         return len(self.nodes)
+
+    def print_table(self):
+        for row in self.table:
+            print('TITLE')
+            print(row[0])
+            print('CONTENT')
+            print(row[1])
