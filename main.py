05d2e08d3ccc99538f68b297a32d4f25a03b47b6
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-11-25 19:40:33 +1100

    Started adding templates for the flake implementaton

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from scraper import Scraper
+from flask import Flask, render_template
 
 scraper = Scraper("https://www.ozbargain.com.au/")
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
 print(scraper.count())
-scraper.print_table()
+# scraper.print_table()
+
+print(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
