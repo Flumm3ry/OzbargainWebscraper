0afb018b3601d67549a971ef212a4a39bcde7d7b
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-11-26 21:11:59 +1100

    Styled listings with cardviews using bootstrap

--- node.py
+++ node.py
@@ -6,18 +6,18 @@ class Node:
         self.deal_link = deal_link
 
     def get_html(self):
-        result = "<br>"
-        result = result + "<p>"
+        result = "<div class='card'><div class='card-body'>"
+        result = result + "<h5 class='card-title'>"
         result = result + self.title
-        result = result + "</p>"
-        result = result + "<p>"
+        result = result + "</h5>"
+        result = result + "<p class='card-text'>"
         result = result + self.content
         result = result + "</p>"
-        result = result + "<p>"
-        result = result + self.op_link
-        result = result + "</p>"
-        result = result + "<p>"
+        result = result + "<a class='card-link' target='_blank' href='"
         result = result + self.deal_link
-        result = result + "</p>"
-        result = result + "<br>"
+        result = result + "'>Link to Deal</a>"
+        result = result + "<a class='card-link' target='_blank' href='"
+        result = result + self.op_link
+        result = result + "'>Link to Original Post</a>"
+        result = result + "</div></div>"
         return result
