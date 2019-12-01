944bd93e9506617b754bd13ed34d2ce594fa8e41
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-12-01 17:03:21 +1100

    Added login page and functionality

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from scraper import Scraper
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from node_list import NodeList
 from apscheduler.schedulers.background import BackgroundScheduler
+import models as dbHandler
 
 url = "https://www.ozbargain.com.au/"
 
-Scraper(url).updateCSV('node_file.txt')
+#Scraper(url).updateCSV('node_file.txt')
 
 node_list = NodeList(filename='node_file.txt')
 
@@ -13,7 +14,7 @@ app = Flask(__name__)
 
 
 def update_scraped_data():
-    Scraper(url).updateCSV('node_file.txt')
+    #Scraper(url).updateCSV('node_file.txt')
     global node_list
     node_list = NodeList('node_file.txt')
 
@@ -31,6 +32,28 @@ def home():
 @app.route("/alerts")
 def alerts():
     return render_template("alerts.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    
+    error = None
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # TODO: Sanitise input here
+        user_id = dbHandler.log_in_user(email, password)
+        if user_id:
+            # Take user to home page and start session here
+            print("logged in")
+            return "User number " + str(user_id) + " logged in"
+        else:
+            print("Wrong details")
+            error = "Incorrect login details"
+
+    # Display form and error message (if any) here
+    return render_template("login.html", err_msg = error)
 
 
 if __name__ == "__main__":
